02e03cdcc0e346ff5e2922cbed527c0b9b435137
DOUHANE Nadim <nadim.douhane@etu.univ-amu.fr>
2025-03-18 17:58:30 +0100

    Ajout image butterfly pour animation. Ainsi que l'annimation

--- app/src/main/java/com/example/qeety/MainActivity.java
+++ app/src/main/java/com/example/qeety/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.qeety;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.drawable.AnimationDrawable;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import java.util.Random;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -13,20 +19,34 @@ import android.os.LocaleList;
 
 import androidx.annotation.RequiresApi;
 
+import com.bumptech.glide.Glide;
+
 import java.util.Locale;
 import java.util.HashMap;
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
 public class MainActivity extends Activity {
+    private final int NUM_BUTTERFLIES = 5; // Nombre de papillons
 
     private HashMap<Integer, String> localeMap; // Associe chaque image à un code de langue
+
+    RelativeLayout mainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainLayout = findViewById(R.id.mainLayout); // Assurez-vous que l'ID est bien défini
+
+        if (mainLayout == null) {
+            throw new NullPointerException("mainLayout is null! Check activity_main.xml.");
+        }
+        generateButterflies();  // Génère plusieurs papillons avec animation
+
         initLocalePicker();
     }
+
 
     private void initLocalePicker() {
         Spinner spinner = findViewById(R.id.localePicker);
@@ -62,4 +82,78 @@ public class MainActivity extends Activity {
             }
         });
     }
+
+    private void generateButterflies2() {
+        Random random = new Random();
+
+        for (int i = 0; i < NUM_BUTTERFLIES; i++) {
+            ImageView butterfly = new ImageView(this);
+            butterfly.setBackgroundResource(R.drawable.butterfly_anim);
+            butterfly.setLayoutParams(new RelativeLayout.LayoutParams(100, 100));
+
+            // Position aléatoire
+            int x = random.nextInt(800);
+            int y = random.nextInt(1200);
+            butterfly.setX(x);
+            butterfly.setY(y);
+
+            mainLayout.addView(butterfly);
+
+            // Démarrer l'animation d'ailes
+            AnimationDrawable butterflyAnimation = (AnimationDrawable) butterfly.getBackground();
+            butterflyAnimation.start();
+
+            // Animation de vol en zigzag
+            ObjectAnimator flyAnimationX = ObjectAnimator.ofFloat(butterfly, "translationX", x, x + 100, x - 100, x);
+            flyAnimationX.setDuration(4000);
+            flyAnimationX.setRepeatCount(ValueAnimator.INFINITE);
+            flyAnimationX.setRepeatMode(ValueAnimator.REVERSE);
+            flyAnimationX.start();
+
+            ObjectAnimator flyAnimationY = ObjectAnimator.ofFloat(butterfly, "translationY", y, y - 200f, y);
+            flyAnimationY.setDuration(3000);
+            flyAnimationY.setRepeatCount(ValueAnimator.INFINITE);
+            flyAnimationY.setRepeatMode(ValueAnimator.REVERSE);
+            flyAnimationY.start();
+        }
+    }
+
+    private void generateButterflies() {
+        Random random = new Random();
+
+        for (int i = 0; i < NUM_BUTTERFLIES; i++) {
+            ImageView butterfly = new ImageView(this);
+            butterfly.setLayoutParams(new RelativeLayout.LayoutParams(200, 200)); // Augmente la taille si nécessaire
+
+            // Charger le GIF avec Glide
+            Glide.with(this)
+                    .asGif()
+                    .load(R.drawable.yellow_butterfly)
+                    .into(butterfly);
+
+            // Position aléatoire
+            int x = random.nextInt(800);
+            int y = random.nextInt(1200);
+            butterfly.setX(x);
+            butterfly.setY(y);
+
+            mainLayout.addView(butterfly);
+
+            // Animation de vol en zigzag
+            ObjectAnimator flyAnimationX = ObjectAnimator.ofFloat(butterfly, "translationX", x, x + 100, x - 100, x);
+            flyAnimationX.setDuration(4000);
+            flyAnimationX.setRepeatCount(ValueAnimator.INFINITE);
+            flyAnimationX.setRepeatMode(ValueAnimator.REVERSE);
+            flyAnimationX.start();
+
+            ObjectAnimator flyAnimationY = ObjectAnimator.ofFloat(butterfly, "translationY", y, y - 200f, y);
+            flyAnimationY.setDuration(3000);
+            flyAnimationY.setRepeatCount(ValueAnimator.INFINITE);
+            flyAnimationY.setRepeatMode(ValueAnimator.REVERSE);
+            flyAnimationY.start();
+        }
+    }
+
+
+
 }
